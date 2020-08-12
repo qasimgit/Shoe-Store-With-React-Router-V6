95fad094bfaffcdd526edf93624154159340256f
qasimgit <qasimpkkhi@gmail.com>
2020-08-12 10:59:46 +0500

    router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
+import 
+  {
+    Router ,
+    Route,
+    Routes,
+    Link,
+    Outlet ,
+    useParams 
+  }  from 'react-router-dom';
+
+function Home() {
+  return (
+   <div>
+     <h1>HEllo Qasim</h1>
+   </div>
+    )
+
+}
 
 function App() {
   return (
-    <div>
-
-    </div>
+   <Router>
+     <Routes>
+       <Route path="/" element={<Home />} />
+     </Routes>
+   </Router>
   );
 }
 
